941150de77c5a49089c46abe9c679fd5ea01c8e9
justinhubert <hubertjustin01@gmail.com>
2025-03-09 12:24:52 +0100

    feat: env

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'))
 
 // Statische HTML-Datei ausliefern
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.listen(port, () => {
